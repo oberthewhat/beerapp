ff63e5467ed091b06706d011b4cb02cf3c400006
John Obertubbesing <oberthewhat@gmail.com>
2020-01-11 20:49:26 -0600

    beer list?

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      beers: null,
+      beers: [],
+      isLiked: false
     };
   }
   componentDidMount() {
+    let beerList = []
     fetch("https://api.punkapi.com/v2/beers")
     .then(response => response.json())
-    .then(d => this.setState({beers: d}))
+    .then(d => this.setState({ beers: d }))
   }
+ 
   render() {
-    return <div></div>
+    console.log(this.state.beers)
+    console.log(this.state.isLiked)
+    return (
+      <div id="maindiv">
+        <h1>Beer List!</h1>
+        <ul>
+          {this.state.beers.map(beer => {
+            return <div className="beerCard"><li key={`beer-${beer.id} `}>{beer.name}<p>{beer.tagline}</p><p>{beer.description}</p><p>{beer.abv}</p>  <button onClick={() => this.setState({ isLiked: true })}>Like!</button></li></div>
+          })}
+        </ul>
+      </div>
+    )
   }
 }
 
