ee71e724fef0aebc2cf0d982d6dd308232d12142
John Obertubbesing <oberthewhat@gmail.com>
2020-01-11 21:23:41 -0600

    Need help, dont want all buttons changing color, only clicked

--- src/App.js
+++ src/App.js
@@ -6,25 +6,38 @@ class App extends React.Component {
     super(props);
     this.state = {
       beers: [],
-      isLiked: false
+      button: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    this.setState(state => ({
+      button: !state.button
+    }));
+  }
+
+
+
   componentDidMount() {
     let beerList = []
     fetch("https://api.punkapi.com/v2/beers")
     .then(response => response.json())
     .then(d => this.setState({ beers: d }))
   }
+
+
  
   render() {
     console.log(this.state.beers)
-    console.log(this.state.isLiked)
+    console.log("button state " +  this.state.button)
+
     return (
       <div id="maindiv">
         <h1>Beer List!</h1>
         <ul>
           {this.state.beers.map(beer => {
-            return <div className="beerCard"><li key={`beer-${beer.id} `}>{beer.name}<p>{beer.tagline}</p><p>{beer.description}</p><p>{beer.abv}</p>  <button onClick={() => this.setState({ isLiked: true })}>Like!</button></li></div>
+            return <div className="beerCard"><li key={`beer-${beer.id} `}>{beer.name}<p>{beer.tagline}</p><p>{beer.description}</p><p>{beer.abv}</p>  <button className={this.state.button ? "buttonTrue": "buttonFalse"} onClick={() => this.setState({ button: true })}>Like!</button></li></div>
           })}
         </ul>
       </div>
